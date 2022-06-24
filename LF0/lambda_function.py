2c1d4013fce2688159c4ea0e6ca0f986339e00e1
20af02 <ammaarfirozi@gmail.com>
2022-06-24 19:47:05 -0400

    Init

---
+++ LF0/lambda_function.py
@@ -0,0 +1,9 @@
+import json
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': "Application under development. Search functionality will be implemented in Assignment 3)"
+    }
